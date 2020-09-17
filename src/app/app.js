578e951006f1b790bd19c1bc6547a43277dacb22
Maciek Sitkowski <msitkowski94@gmail.com>
2020-09-17 16:16:08 +0200

    Initial commit, edit template to fit project purposes

--- src/app/app.js
+++ src/app/app.js
@@ -1,13 +1,44 @@
 import {
-  select
+  select,
+  json,
 } from 'd3';
 
-// Constants
-const 
-  width = 975, 
-  height = 610;
+const kickstarterUrl = 'https://cdn.freecodecamp.org/testable-projects-fcc/data/tree_map/kickstarter-funding-data.json';
+const moviesUrl = 'https://cdn.freecodecamp.org/testable-projects-fcc/data/tree_map/movie-data.json'
+const videoGamesUrl = 'https://cdn.freecodecamp.org/testable-projects-fcc/data/tree_map/video-game-sales-data.json';
 
-// Svg
+// Client dimensions
+const width = document.body.clientWidth;
+const height = document.body.clientHeight;
+
+// Margin convention
+const margin = {
+	top: 20,
+  right: 100,
+  bottom: 20,
+  left: 75,
+};
+const innerWidth = width - margin.left - margin.right;
+const innerHeight = height - margin.top - margin.bottom;
+
+// Select svg and set its dimensions
 const svg = select('svg')
-  .attr('width', width)
-  .attr('height', height);
+  .attr('height', height)
+  .attr('width', width);
+
+// Append group element to svg to complete margin convention
+const g = svg.append('g')
+  .attr('transform', `translate(${margin.left}, ${margin.top})`);
+
+// Fetch data
+Promise.all([
+  json(kickstarterUrl),
+  json(moviesUrl),
+  json(videoGamesUrl)
+]).then(([
+  kickstarterData,
+  moviesData,
+  videoGamesData
+]) => {
+  console.log(kickstarterData, moviesData, videoGamesData)
+})
