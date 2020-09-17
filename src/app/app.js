dccf45c5daeb17c95f14fe5ef1dd859812b9540c
Maciek Sitkowski <msitkowski94@gmail.com>
2020-09-17 16:51:19 +0200

    Create treemap layout and render it on the screen

--- src/app/app.js
+++ src/app/app.js
@@ -1,7 +1,10 @@
 import {
   select,
   json,
+  hierarchy,
+  treemap
 } from 'd3';
+import * as d3 from 'd3';
 
 const kickstarterUrl = 'https://cdn.freecodecamp.org/testable-projects-fcc/data/tree_map/kickstarter-funding-data.json';
 const moviesUrl = 'https://cdn.freecodecamp.org/testable-projects-fcc/data/tree_map/movie-data.json'
@@ -30,6 +33,17 @@ const svg = select('svg')
 const g = svg.append('g')
   .attr('transform', `translate(${margin.left}, ${margin.top})`);
 
+// Create a treemap layout
+const treemapLayout = data => treemap()
+    .size([innerWidth, innerHeight])
+    .padding(1)
+    .round(true)
+  (hierarchy(data)
+    .sum(d => d.value)
+    .sort((a, b) => b.value - a.value))
+
+const color = d3.scaleOrdinal(d3.schemeCategory10)
+
 // Fetch data
 Promise.all([
   json(kickstarterUrl),
@@ -40,5 +54,28 @@ Promise.all([
   moviesData,
   videoGamesData
 ]) => {
-  console.log(kickstarterData, moviesData, videoGamesData)
+  
+  const root = treemapLayout(kickstarterData);
+
+  const leaf = g.selectAll("g")
+    .data(root.leaves())
+    .join("g")
+      .attr("transform", d => `translate(${d.x0},${d.y0})`);
+
+  leaf.append("rect")
+  .attr("fill", d => { while (d.depth > 1) d = d.parent; return color(d.data.name); })
+  .attr("fill-opacity", 0.6)
+  .attr("width", d => d.x1 - d.x0)
+  .attr("height", d => d.y1 - d.y0);
+
+
+
+
+
+
+
+
+
+
+
 })
